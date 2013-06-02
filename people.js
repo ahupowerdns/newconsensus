5f47e9311f14464990938300ad9d001ed463d9fe
bert hubert <bert.hubert@netherlabs.nl>
2013-06-02 21:35:21 +0200

    add several bios, remove duplicate @ from twitters

--- people.js
+++ people.js
@@ -1,6 +1,6 @@
 var newpeople = 
 [
-{ Name: "Mark Sisson", Photo: "MarkSisson.jpg", Twitter: "@Mark_Sisson", 
+{ Name: "Mark Sisson", Photo: "MarkSisson.jpg", Twitter: "Mark_Sisson", 
   Website: "http://www.marksdailyapple.com/#axzz2V2Z92Q00",
   Bio: "A former Ironman competitor and long-distance runner, with a biology \
   degree.  Runs the “Mark’s Daily Apple” blog and is the author of a number \
@@ -17,7 +17,7 @@ var newpeople =
 Name: "Dr John Briffa",
 Website: "http://www.drbriffa.com/",
 Photo: "DrBriffa.jpg",
-Twitter:"@DrBriffa",
+Twitter:"DrBriffa",
 Doctor: true,
 Bio: "An actual UK family doctor (physician, GP etc) who loves his patients. Fights a brave war with UK institutions on their ‘heart healthy advice’ doled out by organizations beholden to the grain industry. Can be a bit wonky at times with posts on ‘grounding’ to leak electrons into the beach, but still an excellent read. Digs up great research, and he must be a godsend to his patients."
 },
@@ -30,7 +30,7 @@ Twitter: ""
 },
 {
 Name: "Dr Thomas Dayspring",
-Twitter: "@DrLipid",
+Twitter: "DrLipid",
 Photo: "DrLipid.jpg",
 Website: "http://www.lecturepad.org/index.php/home/contributing-authors/816-thomas-dayspring-md-facp-fnla-ncmp",
 Doctor: true,
@@ -38,7 +38,7 @@ Bio: "A specialist lipidologist. An expert on all matters of fat in the blood, p
 },
 {
 Name: "Dr Aseem Malhothra",
-Twitter: "@DrAseemMalhotra",
+Twitter: "DrAseemMalhotra",
 Doctor: true,
 Photo: "DrAseemMalhotra.jpg",
 Website: "http://www.guardian.co.uk/profile/aseem-malhotra",
@@ -46,7 +46,7 @@ Bio: "Dr Malhothra is a British cardiologist who thinks our current epidemic of 
 },
 {
 Name: "Yoni Freedhoff",
-Twitter: "@YoniFreedhoff",
+Twitter: "YoniFreedhoff",
 Doctor: true,
 Website: "http://www.weightymatters.ca",
 Photo: "Dr._Freedhoff_bigger.jpg",
@@ -54,7 +54,7 @@ Bio: 'Canadian doctor, assistant professor at University of Ottawa, director of 
 },
 {
 Name: "Jimmy Moore",
-Twitter: "@livinlowcarbman",
+Twitter: "livinlowcarbman",
 Website: "http://www.livinlavidalowcarb.com/",
 Bio: "Jimmy is a great personal example of what one can achieve through the “new consensus”. Runs the prolific Living La Vida Lowcarb podcast, where he interviews the entire “who’s who” of the new consensus. Was so overweight that it might have killed him, which actually happened to his brother. Although he sometimes overstates his own knowledge, and his personal medical situation may make him a “special case”, one can’t help but admire his boundless energy. He’s a central point in the new consensus community.",
 Photo: "JimmyMoore.jpg"
@@ -69,10 +69,33 @@ Bio: "A veterinarian! Peter is driven by the quest for knowledge. Fan of Kwaśni
 },
 {
 Name: "Stephan Guyenet",
-Twitter: "@whsource",
+Twitter: "whsource",
 Website: "http://wholehealthsource.org",
 Bio: "Neurobiologist, researcher. While almost everybody has now settled on the simple conclusion that carbs raise insulin and that insulin causes fat storage, Stephan begs to differ. A controversial figure within the new consensus since he partially agrees, but partially uses ever weirder mouse models to prove insulin is not responsible. I’ve included Stephan because he is a vital part of the new consensus dialogue, even though is he is not part of the actual consensus.",
 Photo: "StephanGuyenet.jpg"
+},
+{
+Name: "“The Fat Nurse”",
+Twitter: "FatNurseBlogger",
+Photo: "TheFatNurse.jpg",
+Doctor: true,
+Website: "http://thefatnurse.wordpress.com/",
+Bio: "Much like Mark Sisson, “The Fat Nurse” provides balanced commentary both on the new consensus, and how the old consensus is being taught in medical school. Spends serious time explaining how things work. Is an actual registered nurse studying to be a primary care clinician and lipid researcher in the future. Each of her blog posts is prime reading material."
+},
+{
+Name: "Ben Goldacre",
+Twitter: "BenGoldacre",
+Doctor: true,
+Photo: "BenGoldacre.jpg",
+Website: "http://www.badscience.net/",
+Bio: "British physician, academic and science writer. As of 2012 he is Wellcome research fellow in epidemiology at the London School of Hygiene and Tropical Medicine. Ben is somewhat of an outlier in this field in that he’s not a specifically “new consensus” guy.  He most certainly is a “things must change” guy though! He’s fighting the medical and especially the pharmaceutical world for their bad pharma. Rightfully criticizes some of the new consensus people for being too sure of their case."
+},
+{
+Name: "Zoë Harcombe",
+Twitter: "ZoeHarcombe",
+Website: "http://www.zoeharcombe.com/",
+Photo: "ZoeHarcombe.jpg",
+Bio: "One of the people criticized by Ben Goldacre. Zoë chanced upon “the truth” of the new consensus via several detours. Used to espouse a vegetarian diet. These days, she advocates a low-carb, low-sugar “real food” diet. Author of a book which describes “The Harcombe Diet”. Even though she could be criticized, she’s doing a world of good. You can’t go wrong advocating “real food” and warning about the dangers of sugar, or the nonsense of lowering cholesterol with statins."
 }
 ]
 
