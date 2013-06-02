caa9aba57bb3d800744deb10266319d1f35eefb3
bert hubert <bert.hubert@netherlabs.nl>
2013-06-02 22:22:20 +0200

    new photos, new people, new content!

--- people.js
+++ people.js
@@ -96,6 +96,89 @@ Twitter: "ZoeHarcombe",
 Website: "http://www.zoeharcombe.com/",
 Photo: "ZoeHarcombe.jpg",
 Bio: "One of the people criticized by Ben Goldacre. Zoë chanced upon “the truth” of the new consensus via several detours. Used to espouse a vegetarian diet. These days, she advocates a low-carb, low-sugar “real food” diet. Author of a book which describes “The Harcombe Diet”. Even though she could be criticized, she’s doing a world of good. You can’t go wrong advocating “real food” and warning about the dangers of sugar, or the nonsense of lowering cholesterol with statins."
+},
+{
+Name: "Tim Noakes",
+Twitter: "ProfTimNoakes",
+Website: "http://www.ssisa.com/pages/science--research/prof-timothy-noakes/?prev=1",
+Photo: "ProfTimNoakes.jpg",
+Bio: "Fascinating South African professor of sports & performance medicine. Literally wrote the book on it. Then, after developing (pre-)diabetes himself, discovered he’d been parroting the untrue “heart healthy carbohydrate” story. Asked his readers to tear out whole chapters of his previous books. Lives a low-carb life these days, and advocates this."
+},
+{
+Name: "William Davis",
+Twitter: "WilliamDavisMD",
+Doctor: true,
+Website: "http://www.wheatbellyblog.com/2011/07/who-is-dr-william-davis/",
+Photo: "WilliamDavisMD.jpg",
+Bio: "What can I say. Dr Davis warns us nonstop about the evils of grain, of which it appears there are many. While I tend to agree, Dr Davis sees grain behind every tree and as the root cause of almost everything. However, it can’t be denied that by now it is clear that such a thing as ‘non-celiac grain intolerance’ actually exists, and that many people can benefit from eliminating grains from their diet. Publishes success stories most Fridays on his blog."
+},
+{
+Name: "Gary Taubes",
+Twitter: "GaryTaubes",
+Website: "http://www.nusi.org/",
+Photo: "GaryTaubes.jpg",
+Bio: "Physicist & journalist, which makes me like him already. Author of “Why we get fat” and “Good calories, bad calories”. Taubes gained prominence in the low-carb diet debate following the publication of his 2002 New York Times Magazine piece \"What if It's All Been a Big Fat Lie?\". Co-founder of the Nutritional Science Institute. Recently published in the respected British Medical Journal about what really makes us fat. With his articles, books and work for NuSI, he’s a pillar of the field."
+},
+{
+Name: "Peter Attia",
+Twitter: "PeterAttiaMD",
+Photo: "PeterAttiaMD.jpg",
+Doctor: true,
+Website: "http://www.nusi.org/",
+Bio: "Trained as a surgeon, but later opted to work for McKinsey & Company. Found that despite exercising and following ‘the old consensus’, he started to gain weight, which he could not get rid of. Decided to study the science, and started “the war on insulin”. Co-founder of the Nutritional Science Institute. NuSi is founded with $35M, and its stated goal is to clarify loads of things about how our body deals with food. Peter also maintains a blog called The Eating Academy, where he’s done a marvelous job on elucidating lipidology, cholesterol, diabetes etc. Worth reading!"
+},
+{
+Name: "Robert Lustig",
+Doctor: true,
+Twitter: '',
+Photo: "RobertLustig.jpg",
+Website: 'http://en.wikipedia.org/wiki/Robert_Lustig',
+Bio: "Robert H. Lustig is an American pediatric endocrinologist at the University of California, San Francisco where he is a Professor of Clinical Pediatrics. Achieved internet fame with his YouTube lecture “<a href=\"http://www.youtube.com/watch?v=dBnniua6-oM\">Sugar: the bitter truth</a>”, which now tops 3.5 million views. Is the main fructose antagonist. Author of a recent <a href=\"http://www.motherjones.com/blue-marble/2013/02/robert-lustic-study-does-sugar-cause-diabetes\">epic paper</a> which argues convincingly that sugar is indeed to blame for the current epidemic of obesity and diabetes."
+},
+{
+Name: "Andreas Eenfeldt",
+Doctor: true,
+Twitter: "DietDoctor1",
+Photo: "AndreasEenfeldt.jpg",
+Website: "http://www.dietdoctor.com",
+Bio: "Swedish medical doctor specialized in family medicine. Wants to find out how to get as healthy as possible using natural methods such as diet, exercise and perhaps a supplement (vitamin D) or two. Sweden is a frontrunner in the world of low-carb. and Andreas is at the forefront of that."
+},
+{
+Name: "Michael R. and Mary Dan Eades",
+Doctor: true,
+Twitter: "DrEades",
+Photo: "MichaelEades.jpg",
+Website: "http://proteinpower.com/drmike/",
+Bio: "Medical doctors and authors of a number of low-carb books of which “Protein Power” is probably the most well known. Maintain an interesting blog where they reflect on recent research. Tweets relevant material almost daily."
+},
+{
+Name: "Richard D. Feinman (‘Richard the Other’)",
+Twitter: "",
+Website: "http://rdfeinman.wordpress.com/",
+Photo: "RichardDFeinman.jpg",
+Bio: "Richard David Feinman is a professor of biochemistry and medical researcher who studies nutrition and metabolism. His current area of research is the area of diet composition and energy balance. He is generally credited for doing the first serious scientific research on the Atkins Diet. His latest work is on how cancer thrives on sugar and insulin, and the effects of very low carb diets on tumors."
+},
+{
+Name: "Tom Naughton",
+Twitter: "TomDNaughton",
+Photo: "TomDNaughton.jpg",
+Website: "http://www.fathead-movie.com/",
+Bio: "Comedian, educator, health writer, software developer. What’s not to like! Saw the ‘Supersize me’ movie, and thought it was a load of bologna. To counteract this funny but inaccurate movie, he decided to make a funny but correct one in response. In “Fathead the movie”, he eats a lot of fast food but still manages to lose weight. In other scenes, renowned experts weigh in on the sorry state of the ‘old consensus’. These days, Tom runs a blog full of interesting information and links."
+},
+{
+Name: "Dr Jay Wortman",
+Doctor: true,
+Twitter: "DrJayWortman",
+Photo: "DrJayWortman.jpg",
+Website: "http://www.drjaywortman.com/",
+Bio: "Dr. Wortman is a physician from Canada. After working in construction for ten years he returned to school to obtain a BSc in Biology and Chemistry and a medical degree. Developed type 2 diabetes himself, dropped all carbs and recovered his health. Canada’s aboriginal populations have been developing rampant diabetes and obesity after following the advice to lay off their arterycloggingsaturatedfats!! of their traditional diets, and to government guidelines. Dr Wortman has made a project of helping these communities return to eating real food that used to work for them. This project was a great success as shown in the <a href=\"http://www.mybigfatdiet.net/\">My Big Fat Diet</a> documentary. In 2003, Dr. Wortman received the National Aboriginal Achievement Award for Medicine. He’s currently reentering active medical service after having been in government for decades. He’s a man to admire!"
+},
+{
+Name: "Ray Medina",
+Twitter: "",
+Website: "http://www.syontix.com",
+Photo: "RayMedina.jpg",
+Bio: "A lot of the new consensus is about the importance of carbs and the right kinds of fat. However, it turns out that within our gut, trillions of bacteria form a vital part of our metabolism. The importance of this ‘hidden organ’ is only now becoming clear. Ray was a long time sufferer of a host of digestive ailments, and after seeing lots of doctors and undergoing many treatments that did not work, he decided to educate himself on all matters of the gut and managed to restore his health.</p><p>This led him to found a company that sells pre- and probiotics, and I don’t know if they are good or not. However, I do know that the Syontix blog is a fount of gut-related wisdom. I feel strongly that when we look back a few decades down the road, we’ll be shaking our heads about how we ignored our gut bacteria “and tried to lose weight by eating less hahaha”."
 }
 ]
 
