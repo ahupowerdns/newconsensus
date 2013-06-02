f85ff061d11746114cb81431e8be52da4a2f8443
bert hubert <bert.hubert@netherlabs.nl>
2013-06-02 21:22:55 +0200

    initial load

---
+++ people.js
@@ -0,0 +1,79 @@
+var newpeople = 
+[
+{ Name: "Mark Sisson", Photo: "MarkSisson.jpg", Twitter: "@Mark_Sisson", 
+  Website: "http://www.marksdailyapple.com/#axzz2V2Z92Q00",
+  Bio: "A former Ironman competitor and long-distance runner, with a biology \
+  degree.  Runs the “Mark’s Daily Apple” blog and is the author of a number \
+  of books describing the ‘primal’ lifestyle, which ranges from how to sleep \
+  to how to barbecue your food.  I admit to being very impressed by Mark.  \
+  His research is impeccable, and he manages to provide clarity and balanced \
+  reviews of even the most controversial issues.  Makes a living from what \
+  he espouses, sells products and books, so has a commercial stake in what \
+  he says.  I admire a man that can outrun almost everybody, can cook with\
+  the best of them, writes books AND debunks medical literature.  Highly  \
+  recommended"
+},
+{
+Name: "Dr John Briffa",
+Website: "http://www.drbriffa.com/",
+Photo: "DrBriffa.jpg",
+Twitter:"@DrBriffa",
+Doctor: true,
+Bio: "An actual UK family doctor (physician, GP etc) who loves his patients. Fights a brave war with UK institutions on their ‘heart healthy advice’ doled out by organizations beholden to the grain industry. Can be a bit wonky at times with posts on ‘grounding’ to leak electrons into the beach, but still an excellent read. Digs up great research, and he must be a godsend to his patients."
+},
+{
+Name: "J. Stanton",
+Photo: "jstanton.jpg",
+Website: "http://www.gnolls.org/",
+Bio: "Author of the Gnoll Credo, a well known paleo book. His story “Eat like a predator, not like prey” nicely brings together a lot of the paleo mindset and what it is based on.",
+Twitter: ""
+},
+{
+Name: "Dr Thomas Dayspring",
+Twitter: "@DrLipid",
+Photo: "DrLipid.jpg",
+Website: "http://www.lecturepad.org/index.php/home/contributing-authors/816-thomas-dayspring-md-facp-fnla-ncmp",
+Doctor: true,
+Bio: "A specialist lipidologist. An expert on all matters of fat in the blood, perhaps one of the best. Believes that carbohydrates cause many issues with blood lipids, and standard bearer that “high cholesterol” is a nonsense diagnosis. Overweight, which is interesting given that he should know how to deal with that! Unlike some of the hardcore ‘low carbing solves everything’, he provides very detailed case studies on when what kind of medication is needed to treat a patient. Has commercial ties to lipid testing industry, speaker for big pharma. Still, he’s an indispensable authority and a great educator."
+},
+{
+Name: "Dr Aseem Malhothra",
+Twitter: "@DrAseemMalhotra",
+Doctor: true,
+Photo: "DrAseemMalhotra.jpg",
+Website: "http://www.guardian.co.uk/profile/aseem-malhotra",
+Bio: "Dr Malhothra is a British cardiologist who thinks our current epidemic of obesity and diabetes (‘Diabesity’) can be prevented. Fights the old consensus with frequent articles in mainstream newspapers. Author of a recent and important paper in the British Medical Journal “The dietary advice on added sugar needs emergency surgery”"
+},
+{
+Name: "Yoni Freedhoff",
+Twitter: "@YoniFreedhoff",
+Doctor: true,
+Website: "http://www.weightymatters.ca",
+Photo: "Dr._Freedhoff_bigger.jpg",
+Bio: 'Canadian doctor, assistant professor at University of Ottawa, director of a weight loss institute. Prolific writer, antagonizes the food industry. Became famous when he got ‘disinvited’ to a food industry event when they found out his opinions. Instead, he put <a href="http://www.youtube.com/watch?v=-BdFkK-HufU&list=UUVNyZUMykNKrK70JC_Q1qug&index=4">his presentation</a> on YouTube where it got a quarter of a million views. Talks a lot about how our environment has become ‘obesogenic’, and how the food industry disingenuously works on keeping us fat. Has a broader view on obesity than many hardcore low-carbers. A force of good.'
+},
+{
+Name: "Jimmy Moore",
+Twitter: "@livinlowcarbman",
+Website: "http://www.livinlavidalowcarb.com/",
+Bio: "Jimmy is a great personal example of what one can achieve through the “new consensus”. Runs the prolific Living La Vida Lowcarb podcast, where he interviews the entire “who’s who” of the new consensus. Was so overweight that it might have killed him, which actually happened to his brother. Although he sometimes overstates his own knowledge, and his personal medical situation may make him a “special case”, one can’t help but admire his boundless energy. He’s a central point in the new consensus community.",
+Photo: "JimmyMoore.jpg"
+},
+{
+Name: "Petro Dobromylskyj",
+Doctor: true,
+Twitter: '',
+Photo: "PetroDobromylskyj.jpg",
+Website: "http://high-fat-nutrition.blogspot.com",
+Bio: "A veterinarian! Peter is driven by the quest for knowledge. Fan of Kwaśniewski’s Optimal Diet, which helped him feel a lot younger and resolve health issues. Peter trawls through large amounts of research, and does a fine job debunking folks trying to tease ‘the old consensus’ out of modern research. Opinionated guy, sometimes overstates the case, but given how much he cares, that is to be understood. Has a gift for seeing non-obvious truths, like for example that “getting fat is good until you stop”. Feuds with Guyenet."
+},
+{
+Name: "Stephan Guyenet",
+Twitter: "@whsource",
+Website: "http://wholehealthsource.org",
+Bio: "Neurobiologist, researcher. While almost everybody has now settled on the simple conclusion that carbs raise insulin and that insulin causes fat storage, Stephan begs to differ. A controversial figure within the new consensus since he partially agrees, but partially uses ever weirder mouse models to prove insulin is not responsible. I’ve included Stephan because he is a vital part of the new consensus dialogue, even though is he is not part of the actual consensus.",
+Photo: "StephanGuyenet.jpg"
+}
+]
+
+
